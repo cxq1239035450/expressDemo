eaa234c50370ba41ab8b4fba69e994dff96300f3
chenxiaoqiang <chenxiaoqiang@dcx.com>
2023-10-23 18:33:44 +0800

    修改提交文件限制

--- app.js
+++ app.js
@@ -2,14 +2,20 @@ var express = require('express');
 var bodyParser = require('body-parser');
 const user = require('./router/user')
 const upload = require('./router/upload')
-
+const fs = require('fs')
 var app = express();
 
 
 // 因为body传递方式为流式 使用插件优化
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
-
+const publicFn = (rej,res,next)=>{
+    const {ip} = rej
+    const url = __dirname +'/log/ip.txt'
+    fs.appendFileSync(url,`${ip} ${new Date()}\n`)
+    next()
+}
+app.use(publicFn)
 // 导出所有public文件
 app.use(express.static(__dirname + '/public'));
 app.use('/user',user)
@@ -23,6 +29,9 @@ app.post('/',(rej,res)=>{
 })
 app.put('/',(rej,res)=>{
     res.send('hello put')
+})
+app.all('*',(rej,res,next)=>{
+    res.send('404 not serve')
 })
 app.listen(80,()=>{
     console.log('启动服务成功');
